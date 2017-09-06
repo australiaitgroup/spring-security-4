cd9ca326d2312b51ad5970a62d8fd7408f80d6f7
Seabook Chen <seabook1024@gmail.com>
2017-09-06 11:07:06 +1000

    Initial setup

---
+++ src/main/java/com/jiangren/j2ee/ss/springsecurity4demo/SpringSecurity4DemoApplication.java
@@ -0,0 +1,12 @@
+package com.jiangren.j2ee.ss.springsecurity4demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity4DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity4DemoApplication.class, args);
+	}
+}
